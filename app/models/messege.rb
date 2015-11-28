bbe75a9ac285140adfb29604ab66370d6c46a340
yuutohosaka <bvanbino@gmail.com>
2015-11-28 11:47:46 +0000

    kadai2 syuusei 3

--- app/models/messege.rb
+++ app/models/messege.rb
@@ -3,4 +3,5 @@ class Messege < ActiveRecord::Base
 
   validates :body, length: {minimum: 2, maximum: 20}
   validates :age, :numericality => {:only_integer => true}
+  validates :age, length: {minimum: 1, maximum: 3}
 end
